16e905d7d029c7e009dbb4a84cad111bd229b1ce
Saha <aveek.saha@hpe.com>
2020-09-04 13:12:25 +0530

    Change init

--- gat.py
+++ gat.py
@@ -1,14 +1,10 @@
 import tensoflow as tf
 
 class Attention(tf.keras.layers.Layer):
-    def __init__(self, units, A, activation=tf.identity, rate=0.0, l2=0.0):
+    def __init__(self, l2=0.0):
         super(Attention, self).__init__()
 
-        self.activation = activation
-        self.units = units
-        self.rate = rate
         self.l2 = l2
-        self.A = A
 
     def build(self, input_shape):
         self.W = self.add_weight(
